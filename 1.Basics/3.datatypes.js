1188c94aaf9e627b27952cf3d128fd55357e75f4
get-some-code <monideepmistry365@gmail.com>
2025-06-24 05:13:15 +0000

    datatypes

---
+++ 1.Basics/3.datatypes.js
@@ -0,0 +1,22 @@
+// alert("Hello") //cannot use this as we are using nodejs not browser
+
+let name = "Moniii" //string
+let age = 19 //integer
+let isLoggedIn = true //boolean
+
+// number => 2 ^ 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (representation of an empty value)
+// undefined => no value is assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof "moniii");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
