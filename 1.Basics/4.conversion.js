1244cb874f4127de4972a5a205c3829a1279e1c0
get-some-code <monideepmistry365@gmail.com>
2025-06-24 05:35:24 +0000

    conversion

---
+++ 1.Basics/4.conversion.js
@@ -0,0 +1,29 @@
+let score = 33
+console.log(typeof score);
+
+score = "33"
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+
+score = "33abc"
+console.log(typeof(Number(score)));
+console.log(Number(score));
+console.log(null);
+
+let loggedIn = true
+loggedIn = 1
+console.log(Boolean(loggedIn));
+
+loggedIn = 0
+console.log(Boolean(loggedIn));
+
+let number = 33
+let stringNumber = String(number)
+
+console.log(typeof stringNumber);
+
+
+
+
